4d8fbe97c1f5bb32da8a7dd648285140fc331a2d
aleee-git <alejandraramirez.niu@nehemiahiu.com>
2023-01-09 08:55:32 -0600

    Asignar props

--- src/componentes/Testimonio.js
+++ src/componentes/Testimonio.js
@@ -2,8 +2,9 @@ import React from "react";
 import "../styles/Testimonios.css"
 
 /* Funcion a mostrar en Main page*/
-/* EXPORTACION NOMBRADA OP. 2 = export function Testimonio () { */
-function Testimonio () {
+/* EXPORTACION NOMBRADA OP. 2 = export function Testimonio () { 
+Agregar la palabra "props" para trabajar con propiedades */
+function Testimonio (props) {
     return (
         /* Componentes de la funcion */
         <div className="cont-testimonio">
@@ -11,8 +12,9 @@ function Testimonio () {
             <img className="img-testimonio" src={require("../images/cat.png")} alt="Cat-1"/>
 
             <div className="info-testimonio">
-                <p className="name-testimonio">Kitty Kitty</p>
-                <p className="cargo-testimonio">Jefe</p>
+                {/* Llamar la propiedad para cada item y accedera a nombre */}
+                <p className="name-testimonio">{props.nombre} en {props.pais} </p>
+                <p className="cargo-testimonio">{props.cargo} en {props.empresa} </p>
                 <p className="text-testimonio">"Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
                 Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
                 when an unknown printer took a galley of type and scrambled it to make a type specimen book"</p>
